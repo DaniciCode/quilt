050987d444139f31e89da322ea23153929010026
Ana Baptista <ana.bandarra@gmail.com>
2022-05-31 13:22:06 +0100

    part of memories controller

--- db/migrate/20220530164232_create_scrapbooks.rb
+++ db/migrate/20220530164232_create_scrapbooks.rb
@@ -1,7 +1,8 @@
 class CreateScrapbooks < ActiveRecord::Migration[6.1]
   def change
     create_table :scrapbooks do |t|
-      t.string :location
+      t.string :scrapbook_title
+      t.text :scrapbook_description
       t.references :user, null: false, foreign_key: true
       t.references :family, null: false, foreign_key: true
 

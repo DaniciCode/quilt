179ff8675f9e695e622aca1314238e4241c6e487
Ana Baptista <ana.bandarra@gmail.com>
2022-05-31 15:22:59 +0100

    index+show

--- app/models/memory.rb
+++ app/models/memory.rb
@@ -1,7 +1,7 @@
 class Memory < ApplicationRecord
   belongs_to :user
   belongs_to :scrapbook_memory
-  has_many :photos
+  has_many_attached :photos
 
   validates :title, :description, :date, :location, presence: true
 end

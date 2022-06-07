ccd52504d0487fec1ca15c938a798e75ce9e0ebf
Ana Baptista <ana.bandarra@gmail.com>
2022-06-07 16:57:34 +0100

    user added

--- app/controllers/user_families_controller.rb
+++ app/controllers/user_families_controller.rb
@@ -7,11 +7,12 @@ class UserFamiliesController < ApplicationController
   end
 
   def create
-    @family = Family.find(params[:family_id])
-    @user = current_user
-    @user_family = UserFamily.create(user: current_user, family: @family)
-    if @user_family.save
-      redirect_to @family
+    family = Family.find(params[:family_id])
+    user = User.find(params[:user_family][:user])
+    user_family = UserFamily.new(user: user, family: family)
+
+    if user_family.save
+      redirect_to family_path(family)
     end
   end
 

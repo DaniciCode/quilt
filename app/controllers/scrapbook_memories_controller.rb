65d284372a19580040b250cfa7b4d26b52e4d32c
Ana Baptista <ana.bandarra@gmail.com>
2022-06-07 12:55:58 +0100

    adding existing memories

--- app/controllers/scrapbook_memories_controller.rb
+++ app/controllers/scrapbook_memories_controller.rb
@@ -1,2 +1,18 @@
 class ScrapbookMemoriesController < ApplicationController
+  def new
+    @family = Family.find(params[:family_id])
+    @scrapbook = Scrapbook.find(params[:scrapbook_id])
+    @scrapbook_memory = ScrapbookMemory.new
+    @memories = current_user.memories
+  end
+
+  def create
+    @memory = Memory.find(params[:scrapbook_memory][:memory])
+    @scrapbook = Scrapbook.find(params[:scrapbook_id])
+    @scrapbook_memory = ScrapbookMemory.create(memory: @memory, scrapbook: @scrapbook)
+    @family = Family.find(params[:family_id])
+    if @scrapbook_memory.save
+      redirect_to family_scrapbook_path(@family, @scrapbook)
+    end
+  end
 end
